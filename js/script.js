40971c92e8460d2dc713cd9fa91627f8d9099dbc
Abisson Santos <abissons@gmail.com>
2021-11-23 10:26:02 -0300

    All right

--- js/script.js
+++ js/script.js
@@ -1,15 +1,60 @@
 var tocar = document.getElementById("play")
 var musica = document.getElementById("o")
 var namemusic = document.getElementById("nome")
-var album = document.querySelector(".image")
+var album = document.querySelector("#img")
+var nameArtista = document.getElementById("Artista")
+var fundoTela = document.getElementById("corpo")
+var voltar = document.getElementById("anterior")
 var proximo = document.getElementById("proximo")
 var nextsong = document.getElementById("s1")
+var indexMusica = 0
 image = document.getElementById("pl")
 var duracaoMusica = document.querySelector(".fim")
 
-duracaoMusica.textContent = segundosparaMinutos(Math.floor(musica.duration))
+var songsCollection = [
+    {nome: "Yellow", artista: "Coldplay", img: "../image/Coldyellow.jpg", src: "../assets/Coldplay  -  Yellow.mp3", fundo: "../image/cold.jpg"},
+    {nome: "Stressed Out", artista: "Twenty One Pilots", img: "../image/stressedout.jpg", src: "../assets/Stressed Out.mp3", fundo: "../image/fundoStressed.jpg"},
+    {nome: "In The End", artista: "Linkin Park", img: "../image/intheend.jpg", src: "../assets/In The End.mp3", fundo: "../image/fundolinkinpark.jpg"},
+    {nome: "The Scientist", artista: "Coldplay", img: "../image/scientist.jpg", src: "../assets/The Scientist.mp3", fundo: "../image/cold.jpg"},
+    {nome: "Let Her Go", artista: "Passenger", img: "../image/lether.jpg", src: "../assets/Let Her Go.mp3", fundo: "../image/lethergo.jpg"},
+    {nome: "Send Me An Angel", artista: "Scorpions", img: "../image/scorpions.jpg", src: "../assets/Send Me An Angel.mp3", fundo: "../image/fundoscorpions.jpg"},
+    {nome: "Bring Me To Life", artista: "Evanescence", img: "../image/bring.jpg", src: "../assets/Bring me to life.mp3", fundo: "../image/fundobring.jpg"},
+    {nome: "Come As You Are", artista: "Nirvana", img: "../image/nirvana.jpg", src: "../assets/Come As You Are.mp3", fundo: "../image/fundonirvana.jpg"}
+]
+loaderMusica(indexMusica)
 
 musica.addEventListener("timeupdate", atualizarBarra)
+
+anterior.addEventListener("click", () => {
+    indexMusica--
+    if(indexMusica < 0){
+        indexMusica = 7
+    }
+    loaderMusica(indexMusica)
+    playMusic()
+})
+
+proximo.addEventListener("click", () => {
+    indexMusica++
+    if(indexMusica > 7){
+        indexMusica = 0
+    }
+    loaderMusica(indexMusica)
+    playMusic()
+})
+
+function loaderMusica(index){
+    musica.setAttribute("src", songsCollection[index].src)
+    musica.addEventListener("loadeddata", () =>{
+        namemusic.textContent = songsCollection[index].nome
+        nameArtista.textContent = songsCollection[index].artista
+        album.src = songsCollection[index].img
+        fundoTela.setAttribute("background", songsCollection[index].fundo)
+        duracaoMusica.textContent = segundosparaMinutos(Math.floor(musica.duration))
+      
+    })
+
+}
 
 function playMusic(){
 
@@ -32,6 +77,7 @@ function atualizarBarra(){
     barra.style.width = Math.floor((musica.currentTime / musica.duration) * 100) + '%'
     let tempoDecorrido = document.querySelector(".inicio")
     tempoDecorrido.textContent = segundosparaMinutos(Math.floor(musica.currentTime))
+    passarMusica(indexMusica)
 }
 function segundosparaMinutos(segundos){
     let campoMinutos = Math.floor(segundos / 60)
@@ -41,9 +87,11 @@ function segundosparaMinutos(segundos){
     }
     return campoMinutos + ':' + campoSegundos
 }
-
-// proximo.addEventListener("click", ()=>{
-//     nextsong.removeAttribute("src")
-//     nextsong.setAttribute("src", "../assets/Let Her Go - Passenger.mp3")
-//     playMusic()
-// })
+function passarMusica(){
+    
+    if(musica.duration == segundosparaMinutos(Math.floor(musica.duration))){
+        indexMusica++
+        loaderMusica(indexMusica)
+        playMusic(indexMusica)
+    }
+}
